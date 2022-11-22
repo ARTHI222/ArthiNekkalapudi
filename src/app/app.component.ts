e6db80ddf7b66c873622174874f0fb6eb8bc0d7f
ARTHI222 <118800166+ARTHI222@users.noreply.github.com>
2022-11-22 14:58:56 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MovieReviewSystem';
+  //console.log(localStorage.getItem("email"));
+  isLoggedIn = !!localStorage.getItem("email");
 }
+
